6a3395df6dc8913cd6e9dc2aa96083ff7c8032e1
N.I <iworksWeb@gmail.com>
2025-01-12 11:12:22 +0900

    Enhance TodoApp tests by adding rendering with QueryClientProvider and initial test case

--- todo-fronend/src/test/components/TodoApp.test.tsx
+++ todo-fronend/src/test/components/TodoApp.test.tsx
@@ -1,3 +1,18 @@
 import { describe } from "vitest";
+import TodoApp from "../../components/TodoApp";
+import { render, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-describe;
+const queryClient = new QueryClient();
+const renderWithClient = (component: React.ReactNode) => {
+  return render(
+    <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
+  );
+};
+
+describe(TodoApp, () => {
+  test("テスト", () => {
+    renderWithClient(<TodoApp />);
+    screen.debug();
+  });
+});
